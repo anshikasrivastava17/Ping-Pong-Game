4e2050efa06870af0ba202dba1f7e96732601f76
anshikasrivastava17 <anshikasrivastava561@gmail.com>
2023-10-17 19:22:50 +0530

    Ball

--- script.js
+++ script.js
@@ -25,6 +25,19 @@ let player2 = {
     velocityY : 0
 }
 
+//ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ball = {            //ball object
+    x : boardWidth/2,  //to centre it horizontally nd vertically
+    y : boardHeight/2,
+    width: ballWidth,
+    height: ballHeight,
+    velocityX : 1,  //moving left & right
+    velocityY : 2   //moving up & down
+}
+
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -45,14 +58,34 @@ function update(){
     context.clearRect(0, 0, board.width, board.height);
     //player1
     context.fillStyle="skyblue";  //fill color of bat as blue
-     player1.y += player1.velocityY;
+
+    //  player1.y += player1.velocityY;
+    //not doing this bcoz it stretches it across the screen without clearing canvas; 
+    //also even if canvas is clean it crosses the screen so need a check for that.
+    //So dont directly update player1. Update it only if it is in bounds
+    let nextPlayer1Y = player1.y + player1.velocityY;
+    if (!outOfBounds(nextPlayer1Y)) {
+        player1.y = nextPlayer1Y;
+    }
     context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
 
-    //player2 - make bat for 2nd player
 
+
+    //player2 - make bat for 2nd player
+    let nextPlayer2Y = player2.y + player2.velocityY;
+    if (!outOfBounds(nextPlayer2Y)) {
+        player2.y = nextPlayer2Y;
+    }
     context.fillRect(player2.x, player2.y, playerWidth, playerHeight);
-    // player2.y += player2.velocityY;  not doing this bcoz it stretches it across the screen without clearing canvas
+    // player2.y += player2.velocityY;  
+    
 }
+
+function outOfBounds(yPosition) {  //func to check bounds so paddle doesnt cross the screen
+    //returns true or false
+    return (yPosition < 0 || yPosition + playerHeight > boardHeight);
+}
+
 
 function movePlayer(e) {
     //player1
