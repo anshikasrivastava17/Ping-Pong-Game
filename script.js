19dddc642a6a32c0322afbff3f777c01a145776f
anshikasrivastava17 <anshikasrivastava561@gmail.com>
2023-10-17 19:07:28 +0530

    Event Listeners for paddle movements

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let context;
 //players (bats' variables for the ball)
 let playerWidth = 10;
 let playerHeight = 50;
-let playerVelocityY = 0;
+let playerVelocityY = 0;  //to make bats move up nd down; only velocityY needed bcoz paddle can go up nd down
 
 let player1 = {  //object
     x : 10,
@@ -33,9 +33,42 @@ window.onload = function() {
 
      //draw initial player1
      context.fillStyle="skyblue";  //fill color of bat as blue
-     context.fillRect(player1.x, player1.y, playerWidth, playerHeight); //make the bat as a rect
+     context.fillRect(player1.x, player1.y, playerWidth, playerHeight); //FOR PLAYER 1 only : make the bat as a rect
      // x y coordinates for position and dimensions for height width of bat
 
-     requestAnimationFrame(update);
+     requestAnimationFrame(update); //func to update positions of bat, when it goes up y coordinates - and vice versa
+     document.addEventListener("keyup", movePlayer);
 }
 
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+    //player1
+    context.fillStyle="skyblue";  //fill color of bat as blue
+     player1.y += player1.velocityY;
+    context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
+
+    //player2 - make bat for 2nd player
+
+    context.fillRect(player2.x, player2.y, playerWidth, playerHeight);
+    // player2.y += player2.velocityY;  not doing this bcoz it stretches it across the screen without clearing canvas
+}
+
+function movePlayer(e) {
+    //player1
+    if (e.code == "KeyW") {  //up means y coordinate decreasing for a board that starts from 0/1
+        player1.velocityY = -3;
+    }
+
+    else if (e.code == "KeyS") {
+        player1.velocityY = 3;
+    }
+
+     //player2
+     if (e.code == "ArrowUp") {
+        player2.velocityY = -3;
+    }
+    else if (e.code == "ArrowDown") {
+        player2.velocityY = 3;
+    }
+}
